3a2bb42daaf4af04969e6e30fce18c6296645bf2
Tripurari Jha <tjha846@gmail.com>
2025-10-05 13:31:45 +0530

    project 1

---
+++ script.js
@@ -0,0 +1,32 @@
+const count = document.querySelector(".count");
+// const add = document.querySelector(".add");
+// const sub = document.querySelector(".sub");
+// const reset = document.querySelector(".reset");
+
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("add")){
+        count.innerHTML = Number(count.innerHTML) +1
+    }
+    else if(e.target.classList.contains("sub")){
+        count.innerHTML = Number(count.innerHTML) - 1
+    }
+    else if(e.target.classList.contains("reset")){
+        count.innerHTML = Number(count.innerHTML = 0)
+    }
+});
+
+
+
+
+
+// add.addEventListener("click", () => {
+//     count.innerHTML = Number(count.innerHTML) +1;
+// })
+// sub.addEventListener("click", () => {
+//     count.innerHTML = Number(count.innerHTML) -1;
+// })
+// reset.addEventListener("click", () => {
+//     count.innerHTML = 0;
+// })
